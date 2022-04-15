15a03a981214f9030e1c05fcdf500e29c33318af
AmosFiona <15663136210@163.com>
2022-04-15 23:03:45 +0800

    Modified lecture and course

--- clean.sh
+++ clean.sh
@@ -7,6 +7,7 @@ rm ./Latex/*.fdb_latexmk
 rm ./Latex/*.fls
 rm ./Latex/*.log
 rm ./Latex/*.synctex.gz
+rm ./Latex/*.synctex\(busy\)
 rm ./Latex/*.xdv
 rm ./Latex/*.pdf
 rm ./Latex/*.toc
@@ -16,16 +17,17 @@ rm ./Latex/COURSE/*.fdb_latexmk
 rm ./Latex/COURSE/*.fls
 rm ./Latex/COURSE/*.log
 rm ./Latex/COURSE/*.synctex.gz
+rm ./Latex/COURSE/*.synctex\(busy\)
 rm ./Latex/COURSE/*.xdv
 rm ./Latex/COURSE/*.pdf
 rm ./Latex/COURSE/*.toc
-
 
 rm ./Latex/LECTURE/*.aux
 rm ./Latex/LECTURE/*.fdb_latexmk
 rm ./Latex/LECTURE/*.fls
 rm ./Latex/LECTURE/*.log
 rm ./Latex/LECTURE/*.synctex.gz
+rm ./Latex/LECTURE/*.synctex\(busy\)
 rm ./Latex/LECTURE/*.xdv
 rm ./Latex/LECTURE/*.pdf
 rm ./Latex/LECTURE/*.toc
@@ -35,6 +37,7 @@ rm ./Latex/examples/*.fdb_latexmk
 rm ./Latex/examples/*.fls
 rm ./Latex/examples/*.log
 rm ./Latex/examples/*.synctex.gz
+rm ./Latex/examples/*.synctex\(busy\)
 rm ./Latex/examples/*.xdv
 rm ./Latex/examples/*.pdf
 rm ./Latex/examples/*.toc
